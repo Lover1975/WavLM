4681fae2fdbb27e54efa253de4127a723ba26870
Lover1975 <drjoenh1983@gmail.com>
2023-10-25 18:35:37 +0000

    Updated Code.

--- main.py
+++ main.py
@@ -6,42 +6,37 @@ import glob
 
 # Load the pre-trained checkpoints
 torch.set_printoptions(threshold=torch.inf)
-checkpoint = torch.load('/mnt/4d55e72b-7ce5-4fc5-b3d4-3cfd1a4a0281/Afshari/Downloads/WavLM_Code/WavLM-Large.pt')
+checkpoint = torch.load('/content/WavLM-Large.pt')
 cfg = WavLMConfig(checkpoint['cfg'])
 model = WavLM(cfg)
 model.load_state_dict(checkpoint['model'])
 model.eval()
 
 # Directory where your .wav files are located (including subdirectories)
-wav_directory = '/mnt/4d55e72b-7ce5-4fc5-b3d4-3cfd1a4a0281/Afshari/vox/vox2_mini_converted'
+wav_directory = '/content/Voxceleb2_small'
 
 # Use glob to find all .wav files in the directory and its subdirectories
 wav_file_paths = glob.glob(os.path.join(wav_directory, '**', '*.wav'), recursive=True)
 
 # Set batch size for processing
 batch_size = 1  # You can adjust this based on your available memory
-
+batch_representations = []
+batch_layer_results = []
+batch_layer_reps = []
 # Process files in batches
 for i in range(0, len(wav_file_paths), batch_size):
-    if i == 2:
-        exit()
     batch_paths = wav_file_paths[i:i + batch_size]
-    print("hello")
-    
-    # Initialize lists to store representations for the batch
-    batch_representations = []
-    batch_layer_results = []
-    batch_layer_reps = []
     
     for wav_file_path in batch_paths:
-        print(f"second loop{wav_file_path}")
+        print("\n")
+        print(f"File number {i} {wav_file_path}")
         # Load the audio file
         waveform, sample_rate = torchaudio.load(wav_file_path)
-        
+        print(f"Waveform shape: {waveform.shape}")
         # Normalize the input if needed
         if cfg.normalize:
             waveform = torch.nn.functional.layer_norm(waveform, waveform.shape)
-        
+            print(f"Waveform shape after normalizatio: {waveform.shape}")
         # Extract representations
         rep, layer_results = model.extract_features(waveform, output_layer=model.cfg.encoder_layers, ret_layer_results=True)[0]
         layer_reps = [x.transpose(0, 1) for x, _ in layer_results]
@@ -50,16 +45,20 @@ for i in range(0, len(wav_file_paths), batch_size):
         batch_representations.append(rep)
         batch_layer_results.append(layer_results)
         batch_layer_reps.append(layer_reps)
+        print(f"rep: {rep.shape}")
+        print(f"layer_results {len(layer_results)}")
+        print(f"layer_reps {len(layer_reps)}")
+        print("\n")
     
     # Process the batch representations as needed
-    for j, wav_file_path in enumerate(batch_paths):
-        print("Representation for", wav_file_path)
-        print("Last Layer Representation:")
-        print(batch_representations[j])
-        print("Layer Results:")
-        print(batch_layer_results[j])
-        print("Layer Representations:")
-        print(batch_layer_reps[j])
+    # for j, wav_file_path in enumerate(batch_paths):
+    #     print("Representation for", wav_file_path)
+    #     print("Last Layer Representation:")
+    #     print(batch_representations[j])
+    #     print("Layer Results:")
+    #     print(batch_layer_results[j])
+    #     print("Layer Representations:")
+    #     print(batch_layer_reps[j])
 
 #import torch
 #print(torch.__version__)
